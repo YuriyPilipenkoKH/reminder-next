208380d5652643aad93d9c015d50e8a0e8d1f501
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-09 20:38:54 +0200

    fix

--- lib/mongoDB.ts
+++ lib/mongoDB.ts
@@ -5,7 +5,7 @@ export const connectMongoDB =async () => {
     console.log(process.env.NEXT_PUBLIC_API_PORT)
     try {
         // const mongoURL = process.env.MONGO_URL
-        const mongoURL = "mongodb+srv://martin:Kh724397@cluster0.pijgqaf.mongodb.net/db_reminder?retryWrites=true&w=majority&appName=Cluster0"
+        const mongoURL = process.env.NEXT_PUBLIC_MONGO_URL
         if (!mongoURL) {
             throw new Error("MongoDB URL is not defined in environment variables");
         }
