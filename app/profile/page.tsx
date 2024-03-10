5d110474c22aabd7cd9678c80158565269ebc1d0
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-10 13:58:41 +0200

    Logout successfull

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,33 @@
+"use client"
+
+import axios from "axios"
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast"
+
+
 function Profile() {
+    const router = useRouter();
+    const logout =async() => {
+        try {
+            const response = await axios.get("/api/users/logout")
+            toast.success('Logout success')
+    
+            console.log("Logout success", response.data)
+            router.push("/login ")                   
+        } 
+        catch (error:any) {
+            console.log("Logout failed",error)
+            toast.error(error.message)
+            
+        }
+    }
     return (
-      <>
-       profile
-      </>
+      <div className="grid place-items-center h-[100vh]">
+       <h1>profile</h1>
+       <button 
+       onClick={logout}
+       className="authbtn w-[200px]">logout</button>
+      </div>
     )
   }
   
--- lib/mongoDB.ts
+++ lib/mongoDB.ts
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectMongoDB =async () => {
-    console.log(process.env.NEXT_PUBLIC_API_DOMAIN)
-    // console.log(process.env.NEXT_PUBLIC_MONGO_URL)
+
     try {
         mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL!);
         const connection = mongoose.connection;
