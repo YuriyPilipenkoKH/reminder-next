602eeb3eeaedd4ed2a7b8bc9a85e9cfc9e147193
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-21 19:55:39 +0200

    home

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
     <main className='grid gap-5 place-items-center h-[400px]'>
         <h1 className='text-bold text-3xl'>Task Reminder </h1>
         <div className='w-[400px] text-xl '>
-            Advanced application reminds you of scheduled appointments and tasks. To-do lists, most important tasks and reminders so you don’t forget anything and have a productive day.
+            Advanced Next.js application  which reminds you of scheduled appointments and tasks, To-do lists, most important tasks and reminders so you don’t forget anything and have a productive day.
         </div>
 
   </main>
