3ec30e89ba8425543aa3b0ea886b7e7edcc0fd75
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-28 12:32:56 +0200

    rem

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,13 +7,14 @@ function HomePage() {
   return (
     <main className='grid gap-5 place-items-center h-[400px]'>
         <H1Text
-        isLarger ={true}
+        // isLarger ={true}
          className='text-bold text-3xl'>
           Task Reminder 
           </H1Text>
         <div className='w-[300px] text-xl '>
             <SText 
-            isLarger ={true}>
+            // isLarger ={true}
+            >
               Advanced Next.js application  which reminds you of scheduled appointments and tasks, To-do lists, most important tasks and reminders so you don’t forget anything and have a productive day.</SText>
         </div>
     </main>
